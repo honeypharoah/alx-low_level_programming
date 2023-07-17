db1ee091175a3193e7b18e9b2058e72ec0d4765e
Sheeren <sheeren.khalifa@outlook.com>
2023-07-16 23:06:06 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
+
 {
 	/* Use the puts function to print the string */
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	/* End the program with a return value of 0 */
-	return 0;
+	return (0);
+
 }
