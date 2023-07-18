09e739892704db27ebb2e63507d06a7c6df6fa21
Sheeren <sheeren.khalifa@outlook.com>
2023-07-18 10:27:03 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef main
+#define main
+
+/**
+ * _putchar - writes the character c to stdoutput
+ * c: the character to print
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c);
+
+#endif
