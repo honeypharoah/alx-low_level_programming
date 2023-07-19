a5ce6e4a4188964efa5fcbfaae56bd70578cb313
Sheeren <sheeren.khalifa@outlook.com>
2023-07-19 05:02:50 -0700

    alphabet checker

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabet character
- * @c: character to check
- * Return: 1 if lower or uppercase, 0 if not alphabet character
+ *_isalpha - checks for alphabetic characters
+ *
+ * @c: the character in ASCII code
+ *
+ * Return: 1 if c is a letter
+ *	otherwise return 0
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 65 && c <= 122)
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
 	}
-	_putchar('\n');
 }
