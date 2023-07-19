bec8bd5642032acf44d6554a018c62f027b77fb6
Sheeren <sheeren.khalifa@outlook.com>
2023-07-19 04:35:11 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
@@ -20,5 +20,5 @@ void print_alphabet_x10(void)
 
 		putchar('\n');
 	}
-	
+
 }
