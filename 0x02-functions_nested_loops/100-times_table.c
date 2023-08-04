a34c6d21e8dc13dfd8867528ed6caeb7ec4d5ad0
Sheeren <sheeren.khalifa@outlook.com>
2023-08-04 11:15:34 -0700

    multiple mains

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - print multiplication table up to n
- * @n: integer argument
+ * print_times_table - print the 9 times table
+ *
+ * @n: times table integer
+ *
+ * Return: no return
  */
 
 void print_times_table(int n)
 {
-	int row;
-	int column;
-	int product;
+        int i;
+        int j;
+        int k;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (row = 0; row <= n; row++)
-		{
-			for (column = 0; column <= n; column++)
-			{
-				product = (row * column);
-				if (column == 0)
-					_putchar('0' + product);
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (product <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-					else if (product > 9 && product < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
-				}
-			}
-			_putchar('\n');
-		}
-	}
-}
+        for (i = 0; i <= n; i++)
+        {
+		if (n < 0 || n > 15)
+		break;
+
+                for (j = 0; j <= n; j++)
+                {
+                        k = i * j;
+
+                        if (j == 0)
+                        {
+                                _putchar(k + '0');
+                        }
+
+                        if (k < 10 && j != 0)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(k + '0');
+                        }
+
+                        else if (k >= 10)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar((k / 10) + '0');
+                                _putchar((k % 10) + '0');
+                        }
+                }
+
+                _putchar('\n');
+"9-times_table.c" 45L, 540C                                        14,0-1        Top
