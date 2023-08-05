f097f4079cbee3ad04ba3af5ab12d5ab74e81114
Sheeren <sheeren.khalifa@outlook.com>
2023-08-05 13:33:16 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,17 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 
 		else if (i % 3 == 0)
                 {
-                        printf("Fizz");
+                        printf("Fizz ");
                 }
 
                 else if (i % 5 == 0)
                 {
-                        printf("Buzz");
+                        printf("Buzz ");
                 }
 
 		else
@@ -32,10 +32,6 @@ int main(void)
 			printf("%d ", i);
 		}
 		
-		if (i < 100)
-		{
-			printf(" ");
-		}
 	}
 
 	printf("\n");
