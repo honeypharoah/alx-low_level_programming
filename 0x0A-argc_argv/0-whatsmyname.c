fc5008c36ce198603559a7b7d7f33ec439ec3566
Sheeren <sheeren.khalifa@outlook.com>
2023-08-07 14:41:09 -0700

    what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: size of argv array
+ * @argv: an array of the size of argc
+ *
+ * Return: always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s \n", argv[0]);
+	}
+
+	return (0);
+}
